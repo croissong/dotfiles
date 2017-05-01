b1df8a2ad0636294a8e6d0dcaa048d9045623220
Jan Moeller <jan.moeller0@gmail.com>
2017-05-01 17:26:27 +0200

    Update borg to use optional archive name

--- borg.sh
+++ borg.sh
@@ -1,9 +1,9 @@
 REPOSITORY=/mnt/synology
-
+name=${1:-'{now:%Y-%m-%d}'}
 # Backup all of /home and /var/www except a few
 # excluded directories
-sudo borg create -v -s -p                          \
-    $REPOSITORY::'{hostname}-{now:%Y-%m-%d}'    \
+borg create -v -s -p                          \
+    $REPOSITORY::"{hostname}-$name"    \
     / \
     -e '/dev/*'                  \
     -e '/proc/*'    \
@@ -12,10 +12,10 @@ sudo borg create -v -s -p                          \
     -e '/run/*' \
     -e '/mnt/*' \
     -e '/media/*' \
-    -e '/lost+found' \
+    -e '/lost+found'
 # Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
 # archives of THIS machine. The '{hostname}-' prefix is very important to
 # limit prune's operation to this machine's archives and not apply to
 # other machine's archives also.s
-boudo borg prune -v --list $REPOSITORY --prefix '{hostname}-' \
+borg prune -v --list $REPOSITORY --prefix '{hostname}-' \
     --keep-daily=7 --keep-weekly=4 --keep-monthly=6
