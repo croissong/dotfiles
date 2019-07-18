bb1dde58f19def27d21c396d86830ae526a7deef
Jan Moeller <jan.moeller0@gmail.com>
2019-07-19 00:26:39 +0200

    (k8s) Update alias; pass+

--- k8s/.my/.shell/k8s.sh
+++ k8s/.my/.shell/k8s.sh
@@ -2,6 +2,6 @@ alias kc=kubectl
 alias kcd='kubectl config set-context $(kubectl config current-context) --namespace '
 alias kctx='kubectl config use-context'
 alias kcg='kubectl get -o yaml'
-alias kcd='kubectl describe'
+alias kcdesc='kubectl describe'
 alias kcroll='kubectl patch deployment -p \
   "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"'
