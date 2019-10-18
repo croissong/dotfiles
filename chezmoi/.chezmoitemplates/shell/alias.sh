8d3e4f7cc1f7f91346f1eb3170febbf60a85d468
Jan Moeller <jan.moeller0@gmail.com>
2019-10-18 14:54:12 +0200

    Fix enhancd find bug

--- chezmoi/.chezmoitemplates/shell/alias.sh
+++ chezmoi/.chezmoitemplates/shell/alias.sh
@@ -2,7 +2,8 @@ alias unpack=dtrx
 alias pack='tar zcvf'
 
 alias ls='exa -gha --group-directories-first'
-alias find='fd -H'
+# https://github.com/b4b4r07/enhancd/issues/104
+# alias find='fd -H'
 alias feh='feh --conversion-timeout 1 -g 640x480 -d -S filename'
 alias top=glances
 alias open=mimeo
