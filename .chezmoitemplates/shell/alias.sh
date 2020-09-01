44a0c9d765f2f0b77fec26aedb10ef243c4298e8
Jan Moeller <jan.moeller0@gmail.com>
2020-09-01 22:56:17 +0200

    Add some wayland env vars; Add htop

--- .chezmoitemplates/shell/alias.sh
+++ .chezmoitemplates/shell/alias.sh
@@ -1,7 +1,7 @@
 alias ls='exa -gha --group-directories-first'
 alias find='fd -H'
 alias feh='feh --conversion-timeout 1 -g 640x480 -d -S filename'
-alias top=glances
+alias top=htop
 alias open=mimeo
 alias history='history -E'
 
