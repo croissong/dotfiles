a3b0af30392e4f076162a9f32190b91a06878d33
Jan Moeller <jan.moeller0@gmail.com>
2021-03-22 13:35:54 +0100

    Update nyxt keybindings

--- dot_config/nyxt/init.lisp
+++ dot_config/nyxt/init.lisp
@@ -1,15 +1,37 @@
 (defvar *my-keymap* (make-keymap "my-map"))
 (define-key *my-keymap*
     "M-d" 'switch-buffer
-    "C-d" 'set-url-new-buffer)
+    "C-q" 'delete-buffer
+    "C-ö" 'switch-buffer-previous
+    "C-#" 'switch-buffer-next
+    "M-ö" 'nyxt/web-mode:history-backwards
+    "M-#" 'nyxt/web-mode:history-forwards
+    "C-M-d" 'set-url-new-buffer
+    "C-d" 'set-url)
+
+(defvar *my-minibuffer-keymap* (make-keymap "my-minibuffer-map"))
+;; (define-key *my-minibuffer-keymap*
+;;     "M-ä" 'nyxt/minibuffer-mode:select-next
+;;     "M-ü" 'nyxt/minibuffer-mode:select-previous)
+
 
 (define-mode my-mode ()
   "Dummy mode for the custom key bindings in `*my-keymap*'."
   ((keymap-scheme (keymap:make-scheme
                    scheme:emacs *my-keymap*))))
 
+;; (define-mode my-minibuffer-mode ()
+;;   "Dummy mode for the custom key bindings in `*my-keymap*'."
+;;   ((keymap-scheme (keymap:make-scheme
+;;                    scheme:emacs *my-minibuffer-keymap*))))
+
+
 ;; (define-configuration buffer
 ;;     ((default-modes (append '() %slot-default))))
 
 (define-configuration (buffer web-buffer)
     ((default-modes (append '(emacs-mode my-mode blocker-mode) %slot-default))))
+
+
+;; (define-configuration (buffer)
+;;     ((default-modes (append '(my-minibuffer-mode) %slot-default))))
