9ceb18de26c723c42872f442b91e41ced1de9ec4
Jan Moeller <jan.moeller0@gmail.com>
2021-04-10 23:52:46 +0200

    Install direnv via zinit

--- shell/zinit.sh
+++ shell/zinit.sh
@@ -4,6 +4,12 @@ ZINIT[HOME_DIR]=$XDG_CONFIG_HOME/zsh/.zinit
 source $ZINIT[HOME_DIR]/bin/zinit.zsh
 
 zinit for \
+    light-mode zinit-zsh/z-a-bin-gem-node \
     light-mode zsh-users/zsh-autosuggestions \
     light-mode zsh-users/zsh-completions \
     light-mode zdharma/fast-syntax-highlighting
+
+zinit from"gh-r" sbin"direnv" mv"direnv* -> direnv" \
+    atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' \
+    src="zhook.zsh" for \
+        direnv/direnv
