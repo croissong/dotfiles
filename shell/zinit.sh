006f08ba7d08ee2f76930eb0ab38a961197f6416
Jan Moeller <jan.moeller0@gmail.com>
2021-04-11 00:15:47 +0200

    Improve zinit loads

--- shell/zinit.sh
+++ shell/zinit.sh
@@ -3,13 +3,17 @@ ZINIT[HOME_DIR]=$XDG_CONFIG_HOME/zsh/.zinit
 
 source $ZINIT[HOME_DIR]/bin/zinit.zsh
 
-zinit for \
-    light-mode zinit-zsh/z-a-bin-gem-node \
-    light-mode zsh-users/zsh-autosuggestions \
-    light-mode zsh-users/zsh-completions \
-    light-mode zdharma/fast-syntax-highlighting
+zinit wait lucid light-mode for \
+	atinit"zicompinit; zicdreplay" \
+	    zdharma/fast-syntax-highlighting \
+	atload"_zsh_autosuggest_start" \
+	    zsh-users/zsh-autosuggestions \
+	blockf atpull'zinit creinstall -q .' \
+	    zsh-users/zsh-completions
+
+zinit light zinit-zsh/z-a-bin-gem-node
 
 zinit from"gh-r" sbin"direnv" mv"direnv* -> direnv" \
-    atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' \
-    src="zhook.zsh" for \
-        direnv/direnv
+	atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' \
+	src="zhook.zsh" for \
+	    direnv/direnv
