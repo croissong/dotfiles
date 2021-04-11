0fd7a59935e5a64d7416c05e29e3f56b9cb4e495
Jan Moeller <jan.moeller0@gmail.com>
2021-04-12 00:30:25 +0200

    Replace tmux w/ tab-rs

--- dot_config/zsh/tmpl/fzf.sh
+++ dot_config/zsh/tmpl/fzf.sh
@@ -3,12 +3,10 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd -t d -d 5"
 
 zinit wait lucid light-mode binary from"gh-r" for \
-    sbin"fzf; fzf-tmux" \
-        atdelete"rm -vf $ZPFX/man/man1/{fzf,fzf-tmux}.1" \
-        dl"https://raw.githubusercontent.com/junegunn/fzf/master/bin/fzf-tmux; \
-            https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh; \
-            https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh; \
-            https://raw.githubusercontent.com/junegunn/fzf/master/man/man1/fzf-tmux.1 -> $ZPFX/man/man1/fzf-tmux.1; \
+    sbin"fzf" \
+        atdelete"rm -vf $ZPFX/man/man1/fzf.1" \
+        dl"https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh; \
+            https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh;
             https://raw.githubusercontent.com/junegunn/fzf/master/man/man1/fzf.1 -> $ZPFX/man/man1/fzf.1" \
         multisrc"completion.zsh key-bindings.zsh" \
         junegunn/fzf
