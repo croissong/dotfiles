1c25466a50d4fc9c7b1566cf477b8187f1f1ad04
Jan Moeller <jan.moeller0@gmail.com>
2021-04-11 20:15:52 +0200

    (zinit) Mv compinit to correct step

--- dot_config/zsh/tmpl/zinit.sh
+++ dot_config/zsh/tmpl/zinit.sh
@@ -1,11 +1,10 @@
 declare -A ZINIT
 ZINIT[HOME_DIR]=$ZDOTDIR/.zinit
+ZINIT[COMPINIT_OPTS]=-C
 
 source $ZINIT[HOME_DIR]/bin/zinit.zsh
 
 zinit wait lucid light-mode for \
-	atinit"zicompinit; zicdreplay" \
-	    zdharma/fast-syntax-highlighting \
 	atload"_zsh_autosuggest_start" \
 	    zsh-users/zsh-autosuggestions \
 	blockf atpull'zinit creinstall -q .' \
@@ -19,3 +18,7 @@ zinit from"gh-r" sbin"direnv" mv"direnv* -> direnv" \
 	atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' \
 	src="zhook.zsh" for \
 	direnv/direnv
+
+zinit wait lucid light-mode for \
+  atinit"zicompinit; zicdreplay" \
+	    zdharma/fast-syntax-highlighting
