4d81a82234da2ad1166ed53a925ef6d192952d5b
Jan Moeller <jan.moeller0@gmail.com>
2021-04-20 23:00:30 +0200

    (zsh) Rm unused, broken keybindings

--- dot_config/zsh/tmpl/keybinds.sh
+++ dot_config/zsh/tmpl/keybinds.sh
@@ -1,8 +1,1 @@
 bindkey -e
-bindkey "^[[3~" delete-char
-bindkey '^[[1;5D' backward-word
-bindkey '^[[1;5C' forward-word
-bindkey '^[ö' backward-word
-bindkey '^[#' forward-word
-bindkey '^[[A' up-line-or-history
-bindkey '^[[1;5A' up-history
