c12b2a0305aea9c74e41172e8d7fa03a50a6c4d3
Jan Moeller <jan.moeller0@gmail.com>
2021-06-26 21:42:11 +0200

    Add fkill fzf func

--- dot_config/zsh/.functions.sh
+++ dot_config/zsh/.functions.sh
@@ -5,3 +5,19 @@ ediff3() {
   fi
   emacsclient -c -q --eval '(ediff-merge-with-ancestor "'$1'" "'$2'" "'$3'")'
 }
+
+
+# https://github.com/junegunn/fzf/wiki/Examples#processes
+fkill() {
+    local pid
+    if [ "$UID" != "0" ]; then
+        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+    else
+        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    fi
+
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+    fi
+}
