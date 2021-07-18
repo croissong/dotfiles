6fc5d49288e83897ff1aeeb5886508da7f213d94
Jan Moeller <jan.moeller0@gmail.com>
2021-07-18 15:02:47 +0200

    Use wezterm as terminal

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,15 @@
+-- https://wezfurlong.org/wezterm/config/files.html
+
+local wezterm = require 'wezterm';
+
 return {
    color_scheme = "Gruvbox Dark",
    enable_tab_bar = false,
    enable_wayland = true,
+   font = wezterm.font("Hack"),
+   font_size = 13,
+
+   colors = {
+      background = "red"
+   }
 }
