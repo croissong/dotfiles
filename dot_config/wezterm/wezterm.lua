ff755c3d85c2ed8b99bfbbfee8c315ac7336303c
Jan Moeller <jan.moeller0@gmail.com>
2021-07-19 08:10:23 +0200

    (wezterm) Customize keybindings

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,13 +3,28 @@
 local wezterm = require 'wezterm';
 
 return {
+   automatically_reload_config = false,
+
    color_scheme = "Gruvbox Dark",
    enable_tab_bar = false,
    enable_wayland = true,
    font = wezterm.font("Hack"),
    font_size = 13,
 
-   colors = {
-      background = "red"
-   }
+   keys = {
+    -- Turn off the default CMD-m Hide action on macOS by making it
+      -- send the empty string instead of hiding the window
+      -- toggle selection mode => x
+      -- "They are not currently reassignable" https://wezfurlong.org/wezterm/copymode.html
+      {key="x", mods="CTRL|ALT", action="ActivateCopyMode"},
+      {key="c", mods="CTRL|ALT", action="Nop"},
+
+      {key="w", mods="ALT", action=wezterm.action{CopyTo="Clipboard"}},
+      {key="y", mods="CTRL", action="Paste"},
+
+      {key="ü", mods="CTRL", action=wezterm.action{ScrollByLine=-20}},
+      {key="ä", mods="CTRL", action=wezterm.action{ScrollByLine=20}},
+
+      {key="r", mods="CTRL|ALT", action="ReloadConfiguration"},
+  }
 }
