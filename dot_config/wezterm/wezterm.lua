71dd6586c50c786db8fda9a8c95220d4815ba099
Jan Moeller <jan.moeller0@gmail.com>
2021-07-19 10:02:59 +0200

    (wezterm) Fix TERM var; Update config; Disable wayland for now
    Wayland clipboard buggy for me

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,10 +4,12 @@ local wezterm = require 'wezterm';
 
 return {
    automatically_reload_config = false,
+   check_for_updates = false,
+   term = "wezterm",
 
    color_scheme = "Gruvbox Dark",
    enable_tab_bar = false,
-   enable_wayland = true,
+   enable_wayland = false,
    font = wezterm.font("Hack"),
    font_size = 13,
 
@@ -17,14 +19,18 @@ return {
       -- toggle selection mode => x
       -- "They are not currently reassignable" https://wezfurlong.org/wezterm/copymode.html
       {key="x", mods="CTRL|ALT", action="ActivateCopyMode"},
+
       {key="c", mods="CTRL|ALT", action="Nop"},
+      {key="Enter", mods="ALT", action="DisableDefaultAssignment"},
 
       {key="w", mods="ALT", action=wezterm.action{CopyTo="Clipboard"}},
-      {key="y", mods="CTRL", action="Paste"},
+      {key="y", mods="CTRL", action=wezterm.action{PasteFrom="Clipboard"}},
 
       {key="ü", mods="CTRL", action=wezterm.action{ScrollByLine=-20}},
       {key="ä", mods="CTRL", action=wezterm.action{ScrollByLine=20}},
 
       {key="r", mods="CTRL|ALT", action="ReloadConfiguration"},
+
+      {key="s", mods="CTRL", action=wezterm.action{Search={CaseInSensitiveString=""}}},
   }
 }
