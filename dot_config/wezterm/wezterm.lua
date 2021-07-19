c5b834956c3b30c179a41a1674b6fa2930243241
Jan Moeller <jan.moeller0@gmail.com>
2021-07-19 11:31:16 +0200

    (wezterm) Update keybindings

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,6 +2,38 @@
 
 local wezterm = require 'wezterm';
 
+
+local keys = {
+   -- Turn off the default CMD-m Hide action on macOS by making it
+   -- send the empty string instead of hiding the window
+   -- toggle selection mode => x
+   -- "They are not currently reassignable" https://wezfurlong.org/wezterm/copymode.html
+   {key="x", mods="CTRL", action="ActivateCopyMode"},
+   {key=" ", mods="CTRL", action="QuickSelect"},
+
+   {key="c", mods="CTRL|ALT", action="Nop"},
+   {key="Enter", mods="ALT", action="DisableDefaultAssignment"},
+
+   {key="w", mods="ALT", action=wezterm.action{CopyTo="Clipboard"}},
+   {key="y", mods="CTRL", action=wezterm.action{PasteFrom="Clipboard"}},
+
+   {key="ü", mods="CTRL", action=wezterm.action{ScrollByLine=-20}},
+   {key="ä", mods="CTRL", action=wezterm.action{ScrollByLine=20}},
+
+   {key="r", mods="CTRL|ALT", action="ReloadConfiguration"},
+
+   {key="s", mods="CTRL", action=wezterm.action{Search={CaseInSensitiveString=""}}},
+}
+
+for i = 1, 8 do
+  -- ALT + number to activate that tab
+  table.insert(keys, {
+    key=tostring(i),
+    mods="ALT",
+    action=wezterm.action{ActivateTab=i-1},
+  })
+end
+
 return {
    automatically_reload_config = false,
    check_for_updates = false,
@@ -12,25 +44,5 @@ return {
    enable_wayland = false,
    font = wezterm.font("Hack"),
    font_size = 13,
-
-   keys = {
-    -- Turn off the default CMD-m Hide action on macOS by making it
-      -- send the empty string instead of hiding the window
-      -- toggle selection mode => x
-      -- "They are not currently reassignable" https://wezfurlong.org/wezterm/copymode.html
-      {key="x", mods="CTRL|ALT", action="ActivateCopyMode"},
-
-      {key="c", mods="CTRL|ALT", action="Nop"},
-      {key="Enter", mods="ALT", action="DisableDefaultAssignment"},
-
-      {key="w", mods="ALT", action=wezterm.action{CopyTo="Clipboard"}},
-      {key="y", mods="CTRL", action=wezterm.action{PasteFrom="Clipboard"}},
-
-      {key="ü", mods="CTRL", action=wezterm.action{ScrollByLine=-20}},
-      {key="ä", mods="CTRL", action=wezterm.action{ScrollByLine=20}},
-
-      {key="r", mods="CTRL|ALT", action="ReloadConfiguration"},
-
-      {key="s", mods="CTRL", action=wezterm.action{Search={CaseInSensitiveString=""}}},
-  }
+   keys = keys,
 }
