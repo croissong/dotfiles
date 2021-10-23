53ed45f45c79af64ba7c2c18639e555531cd19e4
Jan Moeller <jan.moeller0@gmail.com>
2021-10-23 16:45:30 +0200

    (zsh) Disable share history

--- dot_config/zsh/.options.sh
+++ dot_config/zsh/.options.sh
@@ -2,7 +2,7 @@ setopt GLOB_DOTS
 setopt PROMPT_SP
 setopt AUTO_PARAM_SLASH
 setopt AUTO_CD
-setopt SHARE_HISTORY
+# setopt SHARE_HISTORY
 setopt HIST_IGNORE_DUPS
 setopt HIST_FIND_NO_DUPS
 setopt HIST_NO_FUNCTIONS
