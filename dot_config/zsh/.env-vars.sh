683b821e4b30108b656a89ca83ce2c81aa81b1b4
Jan Moeller <jan.moeller0@gmail.com>
2022-01-01 17:48:07 +0100

    feat(zsh): tweak WORDCHARS

--- dot_config/zsh/.env-vars.sh
+++ dot_config/zsh/.env-vars.sh
@@ -4,10 +4,9 @@ HISTFILE=$XDG_CONFIG_HOME/zsh/.histfile
 HISTSIZE=10000
 SAVEHIST=10000
 
-
-# Treat these characters as part of a word.
-WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
-
+# For some reason this gives the best effect
+# from https://stackoverflow.com/questions/444951/zsh-stop-backward-kill-word-on-directory-delimiter
+WORDCHARS=
 
 export SUMMON_PROVIDER=/usr/bin/gopass-summon-provider
 export LS_COLORS='di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
