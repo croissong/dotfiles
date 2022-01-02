0e5b7c11206a3c241a2477b12e6473e8f1a7558c
Jan Moeller <jan.moeller0@gmail.com>
2022-01-02 20:43:19 +0100

    feat(borgmatic): fix gitignore excludes; tweak exclude dirs

--- dot_config/borgmatic/excludes.sh
+++ dot_config/borgmatic/excludes.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-
 rm -f /tmp/excludes.txt
 
 # https://github.com/borgbackup/borg/issues/641
 fd -H '\.gitignore' /home/croissong/code \
-   -E 'golang' \
-   -E 'tmp' \
-   -x rg --color=never '^/?([^\s#].*)$' -r {//}'/*$1' {} \
-   > /tmp/excludes.txt
+  -E 'golang' \
+  -E 'tmp' \
+  -x rg --color=never '^/?([^\s#].*)$' -r sh:{//}'/**/$1' {} \
+  >/tmp/excludes.txt
 
 fd -H '\.gitmodules' /home/croissong/code \
-   -E 'golang' \
-   -E 'tmp' \
-   -x bash -c 'git config --file {} --get-regexp path | choose 1 | rg --color=never "(.*)" -r {//}/\$1' \
-   >> /tmp/excludes.txt
+  -E 'golang' \
+  -E 'tmp' \
+  -x bash -c 'git config --file {} --get-regexp path | choose 1 | rg --color=never "(.*)" -r {//}/\$1' \
+  >>/tmp/excludes.txt
 
-lines=`cat /tmp/excludes.txt | wc -l`
-tput setaf 4; echo "Added $lines lines to excludes"
+lines=$(cat /tmp/excludes.txt | wc -l)
+tput setaf 4
+echo "Added $lines lines to excludes"
