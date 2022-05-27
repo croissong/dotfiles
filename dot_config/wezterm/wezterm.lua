97f5f937b25110313165201fcef485370f375cf9
Jan Moeller <jan.moeller0@gmail.com>
2022-05-27 08:58:46 +0200

    feat(wezterm): add search_mode ClearPattern keybinding; format w/ stylua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 -- https://wezfurlong.org/wezterm/config/files.html
 
-local wezterm = require"wezterm"
+local wezterm = require("wezterm")
 
 ---
 -- Custom functions
@@ -8,8 +8,8 @@ local wezterm = require"wezterm"
 
 -- https://wezfurlong.org/wezterm/config/lua/wezterm/on.html#custom-events
 
-local os = require"os"
-local io = require"io"
+local os = require("os")
+local io = require("io")
 
 wezterm.on("copy-scrollback", function(window, pane)
 	local scrollback = pane:get_lines_as_text(250)
@@ -32,151 +32,193 @@ end)
 ---
 
 local keys = { -- Turn off the default CMD-m Hide action on macOS by making it
--- send the empty string instead of hiding the window
--- toggle selection mode => x
--- "They are not currently reassignable" https://wezfurlong.org/wezterm/copymode.html
-{
-	key = "x",
-	mods = "CTRL",
-	action = "ActivateCopyMode",
-}, {
-	key = " ",
-	mods = "CTRL",
-	action = "QuickSelect",
-}, {
-	key = "c",
-	mods = "CTRL|ALT",
-	action = "Nop",
-}, {
-	key = "Enter",
-	mods = "ALT",
-	action = "DisableDefaultAssignment",
-}, {
-	key = "w",
-	mods = "ALT",
-	action = wezterm.action{ CopyTo = "Clipboard" },
-}, {
-	key = "z",
-	mods = "CTRL",
-	action = wezterm.action{ PasteFrom = "Clipboard" },
-}, {
-	key = "[",
-	mods = "CTRL",
-	action = wezterm.action{ ScrollByPage = -0.5 },
-}, {
-	key = "'",
-	mods = "CTRL",
-	action = wezterm.action{ ScrollByPage = 0.5 },
-}, {
-	key = "r",
-	mods = "CTRL|ALT",
-	action = "ReloadConfiguration",
-}, {
-	key = "s",
-	mods = "CTRL",
-	action = wezterm.action{
-		Search = { CaseInSensitiveString = "" },
+	-- send the empty string instead of hiding the window
+	-- toggle selection mode => x
+	-- "They are not currently reassignable" https://wezfurlong.org/wezterm/copymode.html
+	{
+		key = "x",
+		mods = "CTRL",
+		action = "ActivateCopyMode",
 	},
-}, {
-	key = "q",
-	mods = "CTRL",
-	action = wezterm.action{ShowLauncherArgs={flags="FUZZY|LAUNCH_MENU_ITEMS|KEY_ASSIGNMENTS|DOMAINS"}},
-}, {
-	key = "w",
-	mods = "CTRL|ALT",
-	action = "ShowTabNavigator",
-}, {
-	key = "2",
-	mods = "CTRL",
-	action = wezterm.action{
-		SplitHorizontal = { domain = "CurrentPaneDomain" },
+	{
+		key = " ",
+		mods = "CTRL",
+		action = "QuickSelect",
 	},
-}, {
-	key = "3",
-	mods = "CTRL",
-	action = wezterm.action{
-		SplitVertical = { domain = "CurrentPaneDomain" },
+	{
+		key = "c",
+		mods = "CTRL|ALT",
+		action = "Nop",
 	},
-}, {
-	key = "q",
-	mods = "CTRL|ALT",
-	action = wezterm.action{
-		CloseCurrentPane = { confirm = true },
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = "DisableDefaultAssignment",
 	},
-}, {
-	key = "\\",
-	mods = "CTRL|ALT",
-	action = wezterm.action{ ActivatePaneDirection = "Right" },
-}, {
-	key = ";",
-	mods = "CTRL|ALT",
-	action = wezterm.action{ ActivatePaneDirection = "Left" },
-}, {
-	key = "[",
-	mods = "CTRL|ALT",
-	action = wezterm.action{ ActivatePaneDirection = "Up" },
-}, {
-	key = "'",
-	mods = "CTRL|ALT",
-	action = wezterm.action{ ActivatePaneDirection = "Down" },
-}, {
-	key = "+",
-	mods = "CTRL",
-	action = "IncreaseFontSize",
-}, {
-	key = "e",
-	mods = "CTRL|ALT",
-	action = wezterm.action{ EmitEvent = "copy-scrollback" },
-}, {
-	key = "c",
-	mods = "ALT",
-	action = wezterm.action{ EmitEvent = "copy-cmd" },
-} }
+	{
+		key = "w",
+		mods = "ALT",
+		action = wezterm.action({ CopyTo = "Clipboard" }),
+	},
+	{
+		key = "z",
+		mods = "CTRL",
+		action = wezterm.action({ PasteFrom = "Clipboard" }),
+	},
+	{
+		key = "[",
+		mods = "CTRL",
+		action = wezterm.action({ ScrollByPage = -0.5 }),
+	},
+	{
+		key = "'",
+		mods = "CTRL",
+		action = wezterm.action({ ScrollByPage = 0.5 }),
+	},
+	{
+		key = "r",
+		mods = "CTRL|ALT",
+		action = "ReloadConfiguration",
+	},
+	{
+		key = "s",
+		mods = "CTRL",
+		action = wezterm.action({
+			Search = { CaseInSensitiveString = "" },
+		}),
+	},
+	{
+		key = "q",
+		mods = "CTRL",
+		action = wezterm.action({ ShowLauncherArgs = { flags = "FUZZY|LAUNCH_MENU_ITEMS|KEY_ASSIGNMENTS|DOMAINS" } }),
+	},
+	{
+		key = "w",
+		mods = "CTRL|ALT",
+		action = "ShowTabNavigator",
+	},
+	{
+		key = "2",
+		mods = "CTRL",
+		action = wezterm.action({
+			SplitHorizontal = { domain = "CurrentPaneDomain" },
+		}),
+	},
+	{
+		key = "3",
+		mods = "CTRL",
+		action = wezterm.action({
+			SplitVertical = { domain = "CurrentPaneDomain" },
+		}),
+	},
+	{
+		key = "q",
+		mods = "CTRL|ALT",
+		action = wezterm.action({
+			CloseCurrentPane = { confirm = true },
+		}),
+	},
+	{
+		key = "\\",
+		mods = "CTRL|ALT",
+		action = wezterm.action({ ActivatePaneDirection = "Right" }),
+	},
+	{
+		key = ";",
+		mods = "CTRL|ALT",
+		action = wezterm.action({ ActivatePaneDirection = "Left" }),
+	},
+	{
+		key = "[",
+		mods = "CTRL|ALT",
+		action = wezterm.action({ ActivatePaneDirection = "Up" }),
+	},
+	{
+		key = "'",
+		mods = "CTRL|ALT",
+		action = wezterm.action({ ActivatePaneDirection = "Down" }),
+	},
+	{
+		key = "+",
+		mods = "CTRL",
+		action = "IncreaseFontSize",
+	},
+	{
+		key = "e",
+		mods = "CTRL|ALT",
+		action = wezterm.action({ EmitEvent = "copy-scrollback" }),
+	},
+	{
+		key = "c",
+		mods = "ALT",
+		action = wezterm.action({ EmitEvent = "copy-cmd" }),
+	},
+}
 
 for i = 1, 8 do
 	-- ALT + number to activate that tab
 	table.insert(keys, {
 		key = tostring(i),
 		mods = "ALT",
-		action = wezterm.action{ ActivateTab = i - 1 },
+		action = wezterm.action({ ActivateTab = i - 1 }),
 	})
 end
+
+key_tables = {
+	search_mode = {
+		{ key = "w", mods = "CTRL", action = wezterm.action({ CopyMode = "ClearPattern" }) },
+
+
+    -- TODO: find out how to merge with default key table
+		{ key = "Escape", mods = "NONE", action = wezterm.action({ CopyMode = "Close" }) },
+		{ key = "UpArrow", mods = "NONE", action = wezterm.action({ CopyMode = "PriorMatch" }) },
+		{ key = "Enter", mods = "NONE", action = wezterm.action({ CopyMode = "PriorMatch" }) },
+		{ key = "p", mods = "CTRL", action = wezterm.action({ CopyMode = "PriorMatch" }) },
+		{ key = "PageUp", mods = "NONE", action = wezterm.action({ CopyMode = "PriorMatchPage" }) },
+		{ key = "PageDown", mods = "NONE", action = wezterm.action({ CopyMode = "NextMatchPage" }) },
+		{ key = "n", mods = "CTRL", action = wezterm.action({ CopyMode = "NextMatchPage" }) },
+		{ key = "DownArrow", mods = "NONE", action = wezterm.action({ CopyMode = "NextMatch" }) },
+		{ key = "r", mods = "CTRL", action = wezterm.action({ CopyMode = "CycleMatchType" }) },
+	},
+}
 
 ---
 -- Mouse bindings
 ---
 
 mouse_bindings = { -- Change the default click behavior so that it only selects
--- text and doesn't open hyperlinks
-{
-	event = {
-		Up = {
-			streak = 1,
-			button = "Left",
+	-- text and doesn't open hyperlinks
+	{
+		event = {
+			Up = {
+				streak = 1,
+				button = "Left",
+			},
 		},
+		mods = "NONE",
+		action = wezterm.action({ CompleteSelection = "PrimarySelection" }),
 	},
-	mods = "NONE",
-	action = wezterm.action{ CompleteSelection = "PrimarySelection" },
-}, { -- and make CTRL-Click open hyperlinks
-	event = {
-		Up = {
-			streak = 1,
-			button = "Left",
+	{ -- and make CTRL-Click open hyperlinks
+		event = {
+			Up = {
+				streak = 1,
+				button = "Left",
+			},
 		},
-	},
-	mods = "CTRL",
-	action = "OpenLinkAtMouseCursor",
-}, -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
-{
-	event = {
-		Down = {
-			streak = 1,
-			button = "Left",
+		mods = "CTRL",
+		action = "OpenLinkAtMouseCursor",
+	}, -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+	{
+		event = {
+			Down = {
+				streak = 1,
+				button = "Left",
+			},
 		},
+		mods = "CTRL",
+		action = "Nop",
 	},
-	mods = "CTRL",
-	action = "Nop",
-} }
+}
 
 ---
 -- config
@@ -192,7 +234,8 @@ return {
 	font = wezterm.font("Hack"),
 	font_size = 13,
 	keys = keys,
+	key_tables = key_tables,
 	mouse_bindings = mouse_bindings,
-  debug_key_events = false,
-  key_map_preference = "Physical",
+	debug_key_events = false,
+	key_map_preference = "Physical",
 }
