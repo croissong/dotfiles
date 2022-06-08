32626349412c7d1b049e77d16bb1cfb7b37d5a9a
Jan Moeller <jan.moeller0@gmail.com>
2022-06-08 11:05:23 +0200

    fix(zsh): always use correct gopass-summon-provider executable

--- dot_config/zsh/.env-vars.sh
+++ dot_config/zsh/.env-vars.sh
@@ -8,6 +8,6 @@ SAVEHIST=10000
 # from https://stackoverflow.com/questions/444951/zsh-stop-backward-kill-word-on-directory-delimiter
 WORDCHARS=
 
-export SUMMON_PROVIDER=/usr/bin/gopass-summon-provider
+export SUMMON_PROVIDER=$(which gopass-summon-provider)
 export LS_COLORS='di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
 export GOPASS_NO_NOTIFY=true
