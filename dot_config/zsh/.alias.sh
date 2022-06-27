2b6ce9dfe85e03766707dc3eaba132c7b000ab61
Jan Moeller <jan.moeller0@gmail.com>
2022-06-27 19:47:56 +0200

    fix: fix kubeswitcher binary + alias

--- dot_config/zsh/.alias.sh
+++ dot_config/zsh/.alias.sh
@@ -67,8 +67,8 @@ man() {emacsclient -que "(progn (man \"$1\") (select-frame-set-input-focus (sele
 alias kc=kubectl
 alias kcf=kubectl-fuzzy
 
-alias kctx='switcher'
-alias kcn='switcher ns'
+alias kctx='switch'
+alias kcn='switch ns'
 alias kctxrm='kc config delete-context'
 alias kctxmv='kc config rename-context'
 
