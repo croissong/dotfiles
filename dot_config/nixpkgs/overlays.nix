8e7c217168200d7948cca6d0413015ce235145ca
Jan Moeller <jan.moeller0@gmail.com>
2022-08-28 19:34:15 +0200

    chore(nixpkgs): rm obsolete overlays

--- dot_config/nixpkgs/overlays.nix
+++ dot_config/nixpkgs/overlays.nix
@@ -1,26 +1,4 @@
 [
-
-  (self: super: {
-    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/networking/bukubrow/default.nix
-    bukubrow = let
-      version = "5.4.0";
-      src = super.fetchFromGitHub {
-        owner = "SamHH";
-        repo = super.bukubrow.pname;
-        rev = "v${version}";
-        sha256 = "1a3gqxj6d1shv3w0v9m8x2xr0bvcynchy778yqalxkc3x4vr0nbn";
-      };
-    in (super.bukubrow.override rec {
-      rustPlatform.buildRustPackage = args:
-        super.rustPlatform.buildRustPackage (args
-          // {
-            cargoSha256 = "sha256-CuCiygdBBCmsQ4ZPa8vH6aSxj0+lrh01aHQ6blq9Zg8=";
-            inherit src version;
-            patches = [];
-          });
-    });
-  })
-
   (self: super: {
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/tools/summon/default.nix
     summon = let
@@ -43,25 +21,6 @@
   })
 
   (self: super: {
-    mani = let
-      version = "0.21.0";
-      src = super.fetchFromGitHub {
-        owner = "alajmo";
-        repo = "mani";
-        rev = "v${version}";
-        sha256 = "sha256-eH6V7J0KHGyR//kVr0dOdBYuoR3FDbW/pSh0RhHd4A8=";
-      };
-    in (super.mani.override rec {
-      buildGoModule = args:
-        super.buildGoModule (args
-          // {
-            vendorSha256 = "sha256-g336is8Jjbbzmtsu3suhO9SNq3IJyy1MQ3s8P39MhvU=";
-            inherit src version;
-          });
-    });
-  })
-
-  (self: super: {
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/networking/bukubrow/default.nix
     ouch = let
       version = "0.3.1-next";
@@ -76,27 +35,6 @@
         super.rustPlatform.buildRustPackage (args
           // {
             cargoSha256 = "sha256-RsvpFrwX7QhKpOevH9OnG/E0vRbBpSWLO2j6NUVT/Sg=";
-            inherit src version;
-            patches = [];
-          });
-    });
-  })
-
-  (self: super: {
-    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/networking/cluster/kubelogin/default.nix
-    kubelogin = let
-      version = "0.0.20";
-      src = super.fetchFromGitHub {
-        owner = "Azure";
-        repo = super.kubelogin.pname;
-        rev = "v${version}";
-        sha256 = "sha256-+u+75Z2Efaq16g7kGNq1GHavXwtKvNO6dytniUr8mlE=";
-      };
-    in (super.kubelogin.override rec {
-      buildGoModule = args:
-        super.buildGoModule (args
-          // {
-            vendorSha256 = "sha256-vJfTf9gD/qrsPAfJeMYLjGa90mYLOshgDehv2Fcl6xM=";
             inherit src version;
             patches = [];
           });
