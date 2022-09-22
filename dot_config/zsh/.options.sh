395acb8527eb48e2724a58d9cf475e647d797704
Jan Moeller <jan.moeller0@gmail.com>
2022-09-22 11:50:17 +0200

    feat(zsh): enable INC_APPEND_HISTORY; cleanup opts

--- dot_config/zsh/.options.sh
+++ dot_config/zsh/.options.sh
@@ -1,11 +1,11 @@
-setopt GLOB_DOTS
-setopt PROMPT_SP
+# https://zsh.sourceforge.io/Doc/Release/Options.html
+
 setopt AUTO_PARAM_SLASH
-setopt AUTO_CD
-# setopt SHARE_HISTORY
+# setopt GLOB_DOTS
 setopt HIST_IGNORE_DUPS
 setopt HIST_FIND_NO_DUPS
 setopt HIST_NO_FUNCTIONS
 setopt HIST_REDUCE_BLANKS
+setopt INC_APPEND_HISTORY
 
 unsetopt nomatch
