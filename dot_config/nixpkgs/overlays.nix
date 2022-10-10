18400bbfd29c9185452c83b51bdb7620d23e313b
Jan Moeller <jan.moeller0@gmail.com>
2022-10-10 22:58:59 +0200

    chore(nixpkgs): bump summon overlay version

--- dot_config/nixpkgs/overlays.nix
+++ dot_config/nixpkgs/overlays.nix
@@ -2,18 +2,18 @@
   (self: super: {
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/tools/summon/default.nix
     summon = let
-      version = "0.9.3";
+      version = "0.9.4";
       src = super.fetchFromGitHub {
         owner = "cyberark";
         repo = "summon";
         rev = "v${version}";
-        sha256 = "sha256-3JhQii+G/dGPu6hNzB+/h+udN31YeMQV8anzCXKJVKQ=";
+        sha256 = "sha256-j+3gnrjcMEk5phF0SivjBZBeK7xWjzkAyy4SD3cHDUg=";
       };
     in (super.summon.override rec {
       buildGoModule = args:
         super.buildGoModule (args
           // {
-            vendorSha256 = "sha256-dm5z69dE95ANp7NWL7zlGMMZ11KPB4NzcSbrr4OcJDo=";
+            vendorSha256 = "sha256-ofBqcD/WsUi0XSKydITttsOQja46bQG3Rn1pgsh1b9k=";
             inherit src version;
             patches = [];
           });
