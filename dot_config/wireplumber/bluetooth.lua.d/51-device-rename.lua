a4e107bf87c1a24edf229e7beac180ab6d9d0b69
Jan Moeller <jan.moeller0@gmail.com>
2022-10-24 13:44:32 +0200

    feat: tweak mic setup

---
+++ dot_config/wireplumber/bluetooth.lua.d/51-device-rename.lua
@@ -0,0 +1,12 @@
+-- rule = {
+-- 	matches = {
+-- 		{
+-- 			{ "node.name", "equals", "alsa_input.pci-0000_00_1f.3.analog-stereo" },
+-- 		},
+-- 	},
+-- 	apply_properties = {
+-- 		["node.nick"] = "built-in mic",
+-- 	},
+-- }
+
+-- table.insert(alsa_monitor.rules, rule)
---
+++ dot_config/wireplumber/main.lua.d/51-device-rename.lua
@@ -0,0 +1,24 @@
+rule_rename_built_in_mic = {
+	matches = {
+		{
+			{ "node.name", "equals", "alsa_input.pci-0000_00_1f.3.analog-stereo" },
+		},
+	},
+	apply_properties = {
+		["node.description"] = "built-in mic",
+	},
+}
+
+rule_rename_built_in_speaker = {
+	matches = {
+		{
+			{ "node.name", "equals", "alsa_output.pci-0000_00_1f.3.analog-stereo" },
+		},
+	},
+	apply_properties = {
+		["node.description"] = "built-in speaker",
+	},
+}
+
+table.insert(alsa_monitor.rules, rule_rename_built_in_mic)
+table.insert(alsa_monitor.rules, rule_rename_built_in_speaker)
