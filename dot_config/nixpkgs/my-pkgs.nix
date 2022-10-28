6afe6755c6136336e1838fa81cbea19830158e9e
Jan Moeller <jan.moeller0@gmail.com>
2022-10-28 22:15:21 +0200

    chore(nix): rm obsolete custom xdg-ninja pkg

--- dot_config/nixpkgs/my-pkgs.nix
+++ dot_config/nixpkgs/my-pkgs.nix
@@ -3,23 +3,6 @@
   config,
   ...
 }: let
-  xdg-ninja = pkgs.stdenv.mkDerivation rec {
-    pname = "xdg-ninja";
-    version = "0.2.0.1";
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/b3nj5m1n/xdg-ninja/releases/download/v${version}/xdgnj";
-      sha256 = "sha256-y1BSqKQWbhCyg2sRgMsv8ivmylSUJj6XZ8o+/2oT5ns=";
-    };
-
-    phases = ["installPhase"];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/xdg-ninja
-      chmod +x $out/bin/xdg-ninja
-    '';
-  };
-
   kubesess = pkgs.stdenv.mkDerivation rec {
     pname = "kubesess";
     version = "1.2.8";
@@ -212,7 +195,6 @@
     };
   };
 in {
-  xdg-ninja = xdg-ninja;
   kubesess = kubesess;
   wutag = wutag;
   xmlformatter = xmlformatter;
