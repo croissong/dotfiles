2c3322fd5afadfcf26312603bb70ea33943f227a
Jan Moeller <jan.moeller0@gmail.com>
2022-10-28 22:12:28 +0200

    feat: add mailctl for outlook.office365 auth (wip)

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,37 @@
 module "mail" {
   source = "../priv/tf/mail"
 }
+
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "mail_auth" {
+  display_name     = "mail-auth"
+  owners           = [data.azuread_client_config.current.object_id]
+  sign_in_audience = "AzureADMultipleOrgs"
+}
+
+resource "azuread_application_password" "mail_auth" {
+  application_object_id = azuread_application.mail_auth.object_id
+}
+
+# resource "azuread_service_principal" "mail_auth" {
+#   application_id               = azuread_application.mail_auth.application_id
+#   app_role_assignment_required = false
+#   owners                       = [data.azuread_client_config.current.object_id]
+# }
+
+
+resource "sops_file" "secrets" {
+  encryption_type = "age"
+  content = yamlencode({
+    mail_auth = {
+      client_id     = azuread_application.mail_auth.application_id
+      client_secret = azuread_application_password.mail_auth.value
+    }
+  })
+  filename = "secrets-output.yml"
+  age = {
+    key = "age1d2mr4ka46pms3j042gyshm3jjxth9jq4cje63nvfkzaz5g8e2q6qxrg588"
+  }
+}
--- tf/providers.tf
+++ tf/providers.tf
@@ -9,14 +9,29 @@ terraform {
       source  = "hashicorp/google"
       version = "4.40.0"
     }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.30.0"
+    }
+
+    sops = {
+      source  = "lokkersp/sops"
+      version = "0.6.9"
+    }
   }
 
-  required_version = ">= 1.2"
+  required_version = ">= 1.3"
 }
 
 
+provider "sops" {}
+
 provider "outlook" {}
 
-
-
 provider "google" {}
+
+provider "azuread" {
+  tenant_id = "a30e38d1-62f2-4b94-aea8-f5ae261fd880"
+}
+
