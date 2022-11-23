581463c33dc291110c4a69c69f8dc4d7e05bb58c
Jan Moeller <jan.moeller0@gmail.com>
2022-11-23 08:38:26 +0100

    feat(topgrade): filter some buku bookmarks for ff

--- dot_config/topgrade/executable_buku.sh
+++ dot_config/topgrade/executable_buku.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 folder="$1"
 buku -p -j "${folder}/buku.json"
-jq '[.[] | .["url"] = .uri | .["name"] = .title | del(.uri, .title, .tags, .index, .description)]' "${folder}/buku.json" >"${folder}/buku-firefox-nix.json"
+jq '[.[] | select( .tags | contains("no-export-ff") | not) | .["url"] = .uri | .["name"] = .title | del(.uri, .title, .tags, .index, .description)]' "${folder}/buku.json" >"${folder}/buku-firefox-nix.json"
