4977e351720e97c0ec7c61b7f28c2c6ae0706287
Jan Moeller <jan.moeller0@gmail.com>
2022-11-28 19:58:11 +0100

    chore(nix): rm ouch overlay

--- dot_config/nixpkgs/overlays.nix
+++ dot_config/nixpkgs/overlays.nix
@@ -21,27 +21,6 @@
   })
 
   (self: super: {
-    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/compression/ouch/default.nix
-    ouch = let
-      version = "0.3.1-next";
-      src = super.fetchFromGitHub {
-        owner = "ouch-org";
-        repo = super.ouch.pname;
-        rev = "99ec7d2cf2a929554a3520e393dd80d087935278";
-        sha256 = "sha256-nBDCAb8y8tz4VM7eC+AdF0N2cwoJ/GwqiOo8RhUQs4w=";
-      };
-    in (super.ouch.override rec {
-      rustPlatform.buildRustPackage = args:
-        super.rustPlatform.buildRustPackage (args
-          // {
-            cargoSha256 = "sha256-RsvpFrwX7QhKpOevH9OnG/E0vRbBpSWLO2j6NUVT/Sg=";
-            inherit src version;
-            patches = [];
-          });
-    });
-  })
-
-  (self: super: {
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/networking/cluster/helmfile/default.nix
     helmfile = let
       version = "0.148.1";
