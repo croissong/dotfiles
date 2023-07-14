8d8a388b6f7f35f03daaf33465d852ea8b9614a7
Jan Moeller <jan.moeller0@gmail.com>
2023-07-14 10:19:29 +0200

    feat(zsh): re-enable globdots

--- dot_config/zsh/.options.sh
+++ dot_config/zsh/.options.sh
@@ -1,7 +1,10 @@
 # https://zsh.sourceforge.io/Doc/Release/Options.html
 
 setopt AUTO_PARAM_SLASH
-# setopt GLOB_DOTS
+
+# https://unix.stackexchange.com/questions/308315/how-can-i-configure-zsh-completion-to-show-hidden-files-and-folders
+setopt GLOB_DOTS
+
 setopt HIST_IGNORE_DUPS
 setopt HIST_FIND_NO_DUPS
 setopt HIST_NO_FUNCTIONS
