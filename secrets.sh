dbc1b64506fad7fb732c3aa168259ef0425d94f3
Jan Moeller <jan.moeller0@gmail.com>
2024-01-07 20:55:10 +0100

    fix(secrets): fix file paths

--- secrets.sh
+++ secrets.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 if [ "$1" == "secrets" ]; then
-  sops -d --output-type=json secrets.yml
+  sops -d --output-type=json $SCRIPT_DIR/secrets.yml
 elif [ "$1" == "tf" ]; then
-  sops -d --output-type=json tf/secrets-output.yml
+  sops -d --output-type=json $SCRIPT_DIR/tf/secrets-output.yml
 fi
