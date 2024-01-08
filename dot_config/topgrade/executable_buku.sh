57989eadc6223e7ed92a5c9af63155ad3650ac2f
Jan Moeller <jan.moeller0@gmail.com>
2024-01-08 21:50:18 +0100

    feat(topgrade): add auto commit to buku script

--- dot_config/topgrade/executable_buku.sh
+++ dot_config/topgrade/executable_buku.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-folder="$DOT/priv"
-buku -p -j "${DOT}/priv/buku.json"
-echo "Wrote ${DOT}/priv/buku.json" >&2
+cd ${DOT}/priv
+
+buku -p -j "buku.json"
+echo "Wrote buku export to ${DOT}/priv/buku.json" >&2
+
 jq '[.[] | select( .tags | contains("no-export-ff") | not) | .["url"] = .uri | .["name"] = .title | del(.uri, .title, .tags, .index, .description)]' "${DOT}/priv/buku.json" >"${DOT}/priv/buku-firefox-nix.json"
-git --no-pager -C "${DOT}/priv" diff buku.json
+
+if [[ -n $(git status --porcelain "buku.json") ]]; then
+  git --no-pager diff buku.json
+  git add "buku.json"
+  git commit -m "chore: update buku bookmarks"
+  echo "buku.json changes committed" >&2
+else
+  echo "No changes detected in buku.json" >&2
+fi
