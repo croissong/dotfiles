730f7f7183a13a7d3d7f1b77e36d472759aa5a1b
Jan Moeller <jan.moeller0@gmail.com>
2024-01-18 12:19:03 +0100

    feat: move some zsh settings to hm

--- dot_config/zsh/.env-vars.sh
+++ dot_config/zsh/.env-vars.sh
@@ -1,8 +1,4 @@
-HISTORY_IGNORE="(ls *|pwd)"
 HISTTIMEFORMAT="%d.%m.%y %T "
-HISTFILE=$XDG_CONFIG_HOME/zsh/.histfile
-HISTSIZE=10000
-SAVEHIST=10000
 
 # For some reason this gives the best effect
 # from https://stackoverflow.com/questions/444951/zsh-stop-backward-kill-word-on-directory-delimiter
--- dot_config/zsh/.options.sh
+++ dot_config/zsh/.options.sh
@@ -5,14 +5,12 @@ setopt AUTO_PARAM_SLASH
 # https://unix.stackexchange.com/questions/308315/how-can-i-configure-zsh-completion-to-show-hidden-files-and-folders
 setopt GLOB_DOTS
 
-setopt HIST_IGNORE_DUPS
 setopt HIST_FIND_NO_DUPS
 setopt HIST_NO_FUNCTIONS
 setopt HIST_REDUCE_BLANKS
 setopt INC_APPEND_HISTORY
 
 unsetopt nomatch
-
 
 # https://unix.stackexchange.com/questions/712441/zsh-completion-for-make-only-complete-targets-from-makefile
 zstyle ':completion::complete:make::' tag-order targets variables
