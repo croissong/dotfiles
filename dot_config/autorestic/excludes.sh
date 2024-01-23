1de2b0884dfae8baf79f81ce41cc313049653d6c
Jan Moeller <jan.moeller0@gmail.com>
2024-01-23 12:43:10 +0100

    feat(autorestic): do include git submodules

--- dot_config/autorestic/excludes.sh
+++ dot_config/autorestic/excludes.sh
@@ -8,11 +8,6 @@ fd -H '\.gitignore' ~/code ~/.config ~/Dot ~/System \
   -x rg --color=never '^/?([^\s#].*)$' -r {//}'/**/$1' {} \
   >/tmp/excludes.txt
 
-fd -H '\.gitmodules' ~/code ~/.config ~/Dot ~/System \
-  -E 'golang' \
-  -x bash -c 'git config --file {} --get-regexp path | choose 1 | rg --color=never "(.*)" -r {//}/\$1' \
-  >>/tmp/excludes.txt
-
 lines=$(cat /tmp/excludes.txt | wc -l)
 tput setaf 4
 echo "Added $lines lines to excludes"
