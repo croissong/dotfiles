d2ddf922e53e51dae197699322b39851c0e3da25
Jan Moeller <jan.moeller0@gmail.com>
2024-02-11 17:28:34 +0100

    feat: cleanup  & re-enable wireplumber configs

--- dot_config/wireplumber/bluetooth.lua.d/51-bluez-config.lua
+++
@@ -1,5 +0,0 @@
-bluez_monitor.properties = {
-	-- ["bluez5.enable-sbc-xq"] = true,
-	-- ["bluez5.enable-msbc"] = true,
-	["bluez5.codecs"] = "[aac]",
-}
--- dot_config/wireplumber/main.lua.d/51-device-rename.lua
+++ dot_config/wireplumber/main.lua.d/51-device-rename.lua
@@ -1,23 +1,23 @@
 rule_rename_built_in_mic = {
-	matches = {
-		{
-			{ "node.name", "equals", "alsa_input.pci-0000_00_1f.3.analog-stereo" },
-		},
-	},
-	apply_properties = {
-		["node.description"] = "built-in mic",
-	},
+  matches = {
+    {
+      { "node.name", "equals", "alsa_input.pci-0000_00_1f.3.analog-stereo" },
+    },
+  },
+  apply_properties = {
+    ["node.description"] = "built-in mic",
+  },
 }
 
 rule_rename_built_in_speaker = {
-	matches = {
-		{
-			{ "node.name", "equals", "alsa_output.pci-0000_00_1f.3.analog-stereo" },
-		},
-	},
-	apply_properties = {
-		["node.description"] = "built-in speaker",
-	},
+  matches = {
+    {
+      { "node.name", "equals", "alsa_output.pci-0000_00_1f.3.analog-stereo" },
+    },
+  },
+  apply_properties = {
+    ["node.description"] = "built-in speaker",
+  },
 }
 
 table.insert(alsa_monitor.rules, rule_rename_built_in_mic)
