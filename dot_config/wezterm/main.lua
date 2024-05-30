0a3093ecda07f418c1a1f0344c499a64c76ea9bb
Jan Moeller <jan.moeller0@gmail.com>
2024-05-30 16:07:30 +0200

    refactor(wezterm): use config_builder

--- dot_config/wezterm/main.lua
+++ dot_config/wezterm/main.lua
@@ -1,5 +1,7 @@
 -- https://wezfurlong.org/wezterm/config/files.html
 
+local wezterm = require("wezterm")
+
 ---
 -- Custom functions
 ---
@@ -210,23 +212,24 @@ mouse_bindings = { -- Change the default click behavior so that it only selects
 -- config
 ---
 
-return {
-  automatically_reload_config = false,
-  check_for_updates = false,
+local config = wezterm.config_builder()
+config:set_strict_mode(true)
 
-  -- terminfo installed via https://wezfurlong.org/wezterm/config/lua/config/term.html?highlight=xterm-256color#term--xterm-256color
-  term = "wezterm",
-  color_scheme = "Gruvbox dark, pale (base16)",
-  enable_tab_bar = false,
-  enable_wayland = true,
-  font = wezterm.font("Hack"),
-  font_size = 13,
-  front_end = "WebGpu",
-  keys = keys,
-  key_tables = key_tables,
-  mouse_bindings = mouse_bindings,
-  hyperlink_rules = hyperlink_rules,
-  debug_key_events = false,
-  key_map_preference = "Physical",
-  default_prog = { "fish" },
-}
+config.automatically_reload_config = false
+config.check_for_updates = false
+config.term = "wezterm"
+config.color_scheme = "Gruvbox dark, pale (base16)"
+config.enable_tab_bar = false
+config.enable_wayland = true
+config.font = wezterm.font("Hack")
+config.font_size = 13
+config.front_end = "WebGpu"
+config.keys = keys
+config.key_tables = key_tables
+config.mouse_bindings = mouse_bindings
+config.hyperlink_rules = hyperlink_rules
+config.debug_key_events = false
+config.key_map_preference = "Physical"
+config.default_prog = { "fish" }
+
+return config
