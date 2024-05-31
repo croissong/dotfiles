74b47e585a18b045dea0463bb82bab0c7d1ee1e9
Jan Moeller <jan.moeller0@gmail.com>
2024-06-01 00:29:32 +0200

    feat(wezterm): add some copy_mode keybindings

--- dot_config/wezterm/keys.lua
+++ dot_config/wezterm/keys.lua
@@ -3,6 +3,10 @@ local act = wezterm.action
 
 local functions = require("functions")
 
+function extend(t1, t2)
+  return table.move(t2, 1, #t2, #t1 + 1, t1)
+end
+
 local menus = {
   {
     key = "q",
@@ -96,12 +100,12 @@ local misc = {
 
 local keys = {
   {
-    key = "x",
+    key = "Space",
     mods = "CTRL",
     action = "ActivateCopyMode",
   },
   {
-    key = " ",
+    key = "x",
     mods = "CTRL",
     action = "QuickSelect",
   },
@@ -166,19 +170,45 @@ end
 -- search mode
 
 search_mode = wezterm.gui.default_key_tables().search_mode
-table.insert(search_mode, {
-  key = "w",
-  mods = "CTRL",
-  action = act.CopyMode("ClearPattern"),
-})
+
+search_mode_keys = {
+  {
+    key = "w",
+    mods = "CTRL",
+    action = act.CopyMode("ClearPattern"),
+  },
+}
+
+extend(search_mode, search_mode_keys)
+
+-- copy mode
+
+copy_mode = wezterm.gui.default_key_tables().copy_mode
+
+copy_mode_keys = {
+  {
+    key = "a",
+    mods = "CTRL",
+    action = act.CopyMode("MoveToStartOfLineContent"),
+  },
+  {
+    key = "e",
+    mods = "CTRL",
+    action = act.CopyMode("MoveToEndOfLineContent"),
+  },
+  {
+    key = "Space",
+    mods = "CTRL",
+    action = act.CopyMode({ SetSelectionMode = "Cell" }),
+  },
+}
+
+extend(copy_mode, copy_mode_keys)
 
 key_tables = {
   search_mode = search_mode,
+  copy_mode = copy_mode,
 }
-
-function extend(t1, t2)
-  return table.move(t2, 1, #t2, #t1 + 1, t1)
-end
 
 extend(keys, menus)
 extend(keys, panes)
