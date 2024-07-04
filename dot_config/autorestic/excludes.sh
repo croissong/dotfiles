0780fc5f356726da7f18bbedbf3a9b0e650aa5c3
Jan Moeller <jan.moeller0@gmail.com>
2024-07-04 17:36:50 +0200

    feat(autorestic): update excludes

--- dot_config/autorestic/excludes.sh
+++ dot_config/autorestic/excludes.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+set -uo pipefail
+
 rm -f /tmp/excludes.txt
 
+# https://forum.restic.net/t/skipping-git-ignored-files/4638
+
 # https://github.com/borgbackup/borg/issues/641
-fd -H '\.gitignore' ~/code ~/.config ~/Dot ~/System \
-  -E 'golang' \
-  -x rg --color=never '^/?([^\s#].*)$' -r {//}'/**/$1' {} \
-  >/tmp/excludes.txt
+fd -H '\.gitignore' ~/code ~/.config ~/dot \
+	-E 'golang' \
+	-x rg --color=never '^/?([^\s#].*)$' -r {//}'/**/$1' {} \
+	>/tmp/excludes.txt
 
 lines=$(cat /tmp/excludes.txt | wc -l)
 tput setaf 4
