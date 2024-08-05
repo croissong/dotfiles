f38002a8b7fed71f27236f43c1eeb6be13603a72
Jan Moeller <jan.moeller0@gmail.com>
2024-08-06 00:19:21 +0200

    chore(resticprofile): ignore vscode folders

--- dot_config/resticprofile/executable_git-gc.sh
+++ dot_config/resticprofile/executable_git-gc.sh
@@ -3,11 +3,11 @@
 set -ueo pipefail
 
 fd -H --type directory \
-	-E 'golang' -E '.local' -E .cache -E tmp -E nixpkgs \
+	-E 'golang' -E '.local' -E .cache -E .config/VSCodium -E tmp -E nixpkgs \
 	'\.git$' ~/ \
 	--exec git -C '{//}' gc --auto
 
 fd -H --type directory \
-	-E 'golang' -E '.local' -E .cache -E tmp -E nixpkgs \
+	-E 'golang' -E '.local' -E .cache -E .config/VSCodium -E tmp -E nixpkgs \
 	'\.git$' ~/ \
 	--exec git -C '{//}' submodule foreach git gc --auto
