3b18afe5e8c3ebb6eefb97df1cad8ef839141bb2
Jan Moeller <jan.moeller0@gmail.com>
2024-11-14 17:56:52 +0100

    chore: rm secrets

--- secrets.sh
+++ secrets.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 if [ "$1" == "secrets" ]; then
-  sops -d --output-type=json $SCRIPT_DIR/secrets.yml
+  sops -d --output-type=json "${DOT}/priv/dot/secrets.yml"
 elif [ "$1" == "tf" ]; then
-  sops -d --output-type=json $SCRIPT_DIR/tf/secrets-output.yml
+  sops -d --output-type=json "${DOT}/priv/tf-secrets-output.yml"
 fi
