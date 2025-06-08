0988d189f43a9a53abb403da756a23610a28916b
Jan Moeller <jan.moeller0@gmail.com>
2025-06-08 21:57:26 +0200

    fix(resticprofile): fix generate-excludes.sh for empty gitignore files (2)

--- dot_config/resticprofile/executable_parse-gitignore.sh
+++ dot_config/resticprofile/executable_parse-gitignore.sh
@@ -5,6 +5,6 @@ set -ueo pipefail
 file="$1"
 dir="$2"
 
-if grep -q '[^[:space:]]' "$file"; then
+if rg -q '[^[:space:]]' "$file"; then
   rg --color=never '^/?([^\s#].*)$' -r "$dir"'/**/$1' "$file"
 fi
